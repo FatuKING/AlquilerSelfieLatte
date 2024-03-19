2d3ff34f437ac61603453f566291725af1f6d0d3
fapuking <151843803+fapuking@users.noreply.github.com>
2024-03-19 12:12:16 -0300

    Update Form.jsx

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,7 +1,34 @@
+import React from 'react'
 export function Form () {
   return (
-    <>
-
+    <> <h1>Formulario de alquiler</h1>
+    <form>
+        <label>
+          Nombre de sucursal:
+          <input type="text" name="nombreSucursal" />
+        </label>
+        <label>
+          Nombre de responsable:
+          <input type="text" name="nombreResponsable" />
+        </label>
+        <label>
+          Fecha:
+          <input type="date" name="fecha" />
+        </label>
+        <label>
+          Email:
+          <input type="email" name="email" />
+        </label>
+        <label>
+          Teléfono celular:
+          <input type="tel" name="numeroCelular" />
+        </label>
+        <label>
+          Adjuntar comodato:
+          <input type="file" name="comodato" />
+        </label>
+        <input type="submit" value="Enviar" />
+      </form>
     </>
-  )
+  );
 }
