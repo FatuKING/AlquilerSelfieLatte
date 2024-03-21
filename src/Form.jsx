1150886d344ec3b8036cbc48f169cd77f8d7e6dd
Facundo Cozzani <fcozzani@cafemartinez.central>
2024-03-21 12:25:45 -0300

    no se

--- src/Form.jsx
+++ src/Form.jsx
@@ -2,34 +2,34 @@ export function Form () {
   return (
     <>
       <section className='formBackground'>
-        <form className='form'>
+        <form className='form' action='https://formsubmit.co/hrodriguez@cafemartinez.com.ar' method='POST'>
           <div className='flex justify-between'>
             <h1 className='text-2xl'>Formulario de alquiler</h1>
 
             <button className='text-2xl'>X</button>
           </div>
           <label>
-            <span>Nombre de sucursal</span>
+            <span className='inline-block w-full text-lg'>Nombre de sucursal</span>
             <input className='p-1 h-10 text-black w-full' type='text' name='nombreSucursal' placeholder='Nombre de la sucursal' />
           </label>
           <label>
-            <span>Nombre de responsable</span>
-            <input className='p-1 h-10 text-black w-full' type='text' name='nombreResponsable' placeholder='Nombre de responsable' />
+            <span className='inline-block w-full text-lg'>Nombre de responsable</span>
+            <input className='p-1 h-10 text-black w-full' type='text' name='nombreResponsable' placeholder='Nombre completo' />
           </label>
           <label>
-            <span className='inline-block w-full'>Fecha de inicio </span>
-            <input className='p-1 h-10 text-black w-full' type='date' name='fecha' />
+            <span className='inline-block w-full text-lg'>Email del local </span>
+            <input className='p-1 h-10 text-black w-full' type='email' name='email' placeholder='sumail@cafemartinez.com.ar' />
           </label>
           <label>
-            <span className='inline-block w-full'>Email del local </span>
-            <input className='p-1 h-10 text-black w-full' type='email' name='email' />
-          </label>
-          <label>
-            <span className='inline-block w-full'>Celular</span>
+            <span className='inline-block w-full text-lg'>Celular</span>
             <input className='p-1 h-10 text-black w-full' type='tel' name='numeroCelular' placeholder='Numero de Celular' />
           </label>
           <label>
-            Adjuntar comodato
+            <span className='inline-block w-full text-lg'>Fecha de inicio </span>
+            <input className='p-1 h-10 text-black w-full' type='date' name='fecha' />
+          </label>
+          <label>
+            <span className='inline-block w-full text-lg'>Adjuntar Comodato</span>
             <input className='' type='file' name='comodato' />
           </label>
           <input className='' type='submit' value='Enviar' />
